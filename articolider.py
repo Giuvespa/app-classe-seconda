71ec56c8e53e31d483f4f99e88b5ea51eca43c58
Giuvespa <giusssab@gmail.com>
2026-01-30 22:20:53 +0100

    Add files via upload

---
+++ articolider.py
@@ -0,0 +1,13 @@
+st.header("🕵️ Missione: I Cacciatori di Articoli")
+
+with st.expander("📖 Leggi il compito di oggi", expanded=True):
+    st.write("""
+    **Istruzioni:**
+    Sul tuo quaderno, scrivi l'articolo determinativo giusto davanti a queste parole:
+    - ... zaino
+    - ... gatta
+    - ... alberi
+    - ... isola
+    
+    Poi scatta una foto e inviamela! 📸
+    """)
